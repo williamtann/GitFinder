330904b356b3b2d035588a240a57e3a6031e43d5
williamtann <william.william@traveloka.com>
2020-05-21 12:26:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gitfinder/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.gitfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.gitfinder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        if (!viewModel.keyword.isNullOrEmpty()) {
+            binding.textView.text = "Searching with keyword: ${viewModel.keyword}"
+        }
+
+        binding.buttonSearch.setOnClickListener {
+            val inputtedValue = binding.editText.text.toString().trim()
+            binding.textView.text = "Searching with keyword: $inputtedValue"
+            viewModel.keyword = inputtedValue
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gitfinder/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.gitfinder
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var keyword: String? = null
+}
