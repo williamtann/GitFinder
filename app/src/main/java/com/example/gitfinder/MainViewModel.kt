ecc710e7c49db0d06774e50c5b4a51f067c4fff1
williamtann <william.william@traveloka.com>
2020-07-04 16:42:46 +0700

    1

--- app/src/main/java/com/example/gitfinder/MainViewModel.kt
+++ app/src/main/java/com/example/gitfinder/MainViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.gitfinder
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
-    var keyword: String? = null
+//    var keyword: String? = null
+
+    private val _keyword: MutableLiveData<String> = MutableLiveData()
+    val keyword: LiveData<String>
+        get() = _keyword
+
+    fun updateKeyword(input: String) {
+        _keyword.value = input
+    }
 }
