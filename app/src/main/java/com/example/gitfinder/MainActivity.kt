2d2af77854c5c8b34cc80d5b0cf00ab2b73dbf95
williamtann <william.william@traveloka.com>
2020-07-06 18:28:35 +0700

    Revert "LiveData"
    This reverts commit 13213e54

--- app/src/main/java/com/example/gitfinder/MainActivity.kt
+++ app/src/main/java/com/example/gitfinder/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.example.gitfinder
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.gitfinder.databinding.ActivityMainBinding
 
@@ -23,41 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        if (!viewModel.keyword.isNullOrEmpty()) {
+            binding.textView.text = "Searching with keyword: ${viewModel.keyword}"
+        }
+
         binding.buttonSearch.setOnClickListener {
-            val textInput = binding.editText.text.toString().trim()
-            viewModel.search(textInput)
+            val inputtedValue = binding.editText.text.toString().trim()
+            binding.textView.text = "Searching with keyword: $inputtedValue"
+            viewModel.keyword = inputtedValue
         }
-
-        initObservation()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.history) {
-            binding.textHistory.visibility =
-                if (binding.textHistory.visibility == View.GONE) View.VISIBLE
-                else View.GONE
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun initObservation() {
-        viewModel.searchEnabled.observe(this, Observer {
-            if (it) {
-                binding.textView.text = "Searching repo with keyword: ${viewModel.keyword.value}"
-            } else {
-                Toast.makeText(this, "Please input the search keyword", Toast.LENGTH_SHORT).show()
-            }
-        })
-        viewModel.searchResult.observe(this, Observer {
-            binding.textView.text = "Repo found: $it"
-        })
-        viewModel.searchHistory.observe(this, Observer {
-            binding.textHistory.text = it
-        })
     }
 }
