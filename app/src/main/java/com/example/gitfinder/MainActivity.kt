f1ecd87860aa177ea3b30536b283101cff1eed34
williamtann <william.william@traveloka.com>
2020-07-06 18:28:16 +0700

    Revert "Retrofit"
    This reverts commit 4aabbcb1

--- app/src/main/java/com/example/gitfinder/MainActivity.kt
+++ app/src/main/java/com/example/gitfinder/MainActivity.kt
@@ -8,17 +8,13 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.gitfinder.adapter.RepoListAdapter
 import com.example.gitfinder.databinding.ActivityMainBinding
-import com.example.gitfinder.datamodel.Repo
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
-    private lateinit var listAdapter: RepoListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,16 +22,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        listAdapter = RepoListAdapter(this, object : RepoListAdapter.ItemClickListener {
-
-            override fun onItemClick(repo: Repo) {
-                Toast.makeText(this@MainActivity, "Repo clicked: ${repo.name}", Toast.LENGTH_SHORT).show()
-            }
-        })
-        binding.recyclerView.adapter = listAdapter
-        binding.recyclerView.itemAnimator = null
 
         binding.buttonSearch.setOnClickListener {
             val textInput = binding.editText.text.toString().trim()
@@ -62,28 +48,16 @@ class MainActivity : AppCompatActivity() {
     private fun initObservation() {
         viewModel.searchEnabled.observe(this, Observer {
             if (it) {
-                binding.recyclerView.visibility = View.GONE
-                binding.textView.visibility = View.VISIBLE
                 binding.textView.text = "Searching repo with keyword: ${viewModel.keyword.value}"
             } else {
                 Toast.makeText(this, "Please input the search keyword", Toast.LENGTH_SHORT).show()
             }
         })
+        viewModel.searchResult.observe(this, Observer {
+            binding.textView.text = "Repo found: $it"
+        })
         viewModel.searchHistory.observe(this, Observer {
             binding.textHistory.text = it
-        })
-        viewModel.reposFound.observe(this, Observer {
-            if (it.isNotEmpty()) {
-                binding.recyclerView.visibility = View.VISIBLE
-                binding.textView.visibility = View.GONE
-                listAdapter.submitList(it)
-            } else {
-                binding.textView.text = "No result found, please search with another keyword"
-            }
-        })
-        viewModel.networkError.observe(this, Observer {
-            binding.textView.visibility = View.GONE
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
     }
 }
--- app/src/main/java/com/example/gitfinder/datamodel/RepoSearchResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gitfinder.datamodel
-
-import com.google.gson.annotations.SerializedName
-
-data class RepoSearchResponse(
-    @SerializedName("items") val items: List<Repo> = emptyList()
-)
