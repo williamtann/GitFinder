52d081d878d7b59150d1ea4aef810912d019aa2c
williamtann <william.william@traveloka.com>
2020-07-10 20:24:08 +0700

    Adjustments

--- app/src/main/java/com/example/gitfinder/viewmodel/RepoSearchResult.kt
+++ app/src/main/java/com/example/gitfinder/viewmodel/RepoSearchResult.kt
@@ -4,6 +4,6 @@ import androidx.lifecycle.LiveData
 import com.example.gitfinder.datamodel.Repo
 
 data class RepoSearchResult (
-    val data: LiveData<List<Repo>>?,
+    val data: LiveData<List<Repo>>,
     val networkError: LiveData<String>
 )
